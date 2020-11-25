3af808ed3251c661746f1b8409a0e8e4b5a5831c
theanarkh <2923878201@qq.com>
2020-11-26 00:09:20 +0800

    'm'

--- client.js
+++ client.js
@@ -1,11 +1,15 @@
 const { processRequests, requestInterval, workerPath } = require('./config');
 const request = require(workerPath);
+let nextStart; 
 function worker() {
+  const start = Date.now();
   for (let i = 0; i < processRequests; i++) {
     request(); 
   }
+  const detal = Date.now() - start;
+  nextStart = requestInterval - (detal / 1000) * 1000 - detal % 1000;
 }
 worker();
 setInterval(() => {
   worker(); 
-}, requestInterval);
+}, nextStart);
